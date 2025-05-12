4027efc9310d676d71062f15572cc00bb20b5c6e
Florian <f.rumigny@gmail.com>
2025-05-12 10:06:32 +0200

    setup tailwind/shadcn + header done

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 export function Header() {
   return (
-    <header className="header">
-      <h1>Checkpoint : frontend</h1>
-      <Link to="/">Countries</Link>
+    <header className="header text-center bg-rose-500 text-white py-5">
+      <h1 className=" text-2xl pb-3">Checkpoint : frontend</h1>
+      <Link to="/" className=" text-sm">Countries</Link>
     </header>
   );
 }
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,3 +1,3 @@
 export function HomePage() {
-  return <p>You should start working here!</p>;
+  return <p className=" text-red-700">You should start working here!</p>;
 }
